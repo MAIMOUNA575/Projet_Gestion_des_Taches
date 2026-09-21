30fe9d7dfaaccb07a1748764ada49ea7376021d0
Mouna <coulmouna001@gmail.com.com>
2026-09-21 15:28:06 +0100

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,16 @@
 import { Module } from "@nestjs/common";
-import { createObserveModule } from "@nestjs/observe";
 import { AppController } from "./app.controller.js";
 import { AppService } from "./app.service.js";
+import {AuthModule} from "@thallesp/nestjs-better-auth";
+import {auth} from "./lib/auth.js";
 
-export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
 @Module({
 	imports: [
 		// Distributed tracing, auto-correlated logs, request/job metrics, error
 		// telemetry, alarms, and more — out of the box. Sign up at https://observe.nestjs.com
-		ObserveModule.forRoot({
-			appKey: "YOUR_APP_KEY",
-			appSecret: "YOUR_APP_SECRET",
-			serviceId: "projet_gestion_taches",
-		}),
+		
+		AuthModule.forRoot(auth),
 	],
 	controllers: [AppController],
 	providers: [AppService],
---
+++ src/lib/auth.ts
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "../generated/prisma/client.js";
+
+const databaseUrl = process.env.DATABASE_URL;
+if(!databaseUrl){
+    throw new Error("DATABASE_URL is not set");
+}
+const adapter = new PrismaPg({ connectionString:databaseUrl});
+const prisma = new PrismaClient({adapter});
+
+export const auth =  betterAuth({
+    database: prismaAdapter(prisma,{
+        provider: "postgresql"
+    }),
+    emailAndPassword: {
+        enabled:true,
+    },
+});
